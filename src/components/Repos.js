c36c4dd179005a2fa7840bdd5d6862e157d57ab8
Yashar Akish <yashar_akish@yahoo.com>
2022-03-09 21:25:31 -0500

    dispaying chart from fusionchart

--- src/components/Repos.js
+++ src/components/Repos.js
@@ -2,8 +2,12 @@ import React from 'react';
 import styled from 'styled-components';
 import { GithubContext } from '../context/context';
 import { ExampleChart, Pie3D, Column3D, Bar3D, Doughnut2D } from './Charts';
+
 const Repos = () => {
-  return <h2>repos component</h2>;
+  const {repos} = React.useContext(GithubContext);
+
+  
+  return <ExampleChart />;
 };
 
 const Wrapper = styled.div`
